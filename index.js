9e7172bbffde523594b7059392303d58d2494601
martasvobo <martasvobo@gmail.com>
2022-06-16 17:17:10 +0300

    changed functionality to match given letter

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post("/", (req, res) => {
-  const word = req.body.word?.replace(/o/g, "") || "Bad request";
-  res.send(word);
+  const letter = req.body?.letter;
+  let word = req.body?.word;
+  if (letter && word) {
+    const regex = new RegExp(letter, "gi");
+    word = word.replace(regex, "");
+    res.send(word);
+  } else {
+    res.send("Bad request");
+  }
 });
 
 app.listen(port);
